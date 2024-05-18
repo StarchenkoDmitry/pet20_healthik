656a51b80946e4aae8b030f6e163175cb8c84048
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-05-18 21:56:54 +0300

    add ConfigModule in App module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BotModule } from './modules/bot/bot.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [BotModule],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+    }),
+    BotModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
