e74d06a45f2d0c9393ab3ef4947a26b9ec1b57a8
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-05-19 16:43:38 +0300

    add createTranslator

---
+++ server/src/i18n/translator.ts
@@ -0,0 +1,25 @@
+import { I18n, LocaleNegotiator } from '@grammyjs/i18n';
+import { FluentVariable } from '@grammyjs/i18n/script/src/deps';
+import { Context } from 'grammy';
+
+export function createTranslator<TContext extends Context>({
+  localeNegotiator,
+  globalTranslationContext,
+  directory,
+  defaultLocale,
+}: {
+  localeNegotiator: LocaleNegotiator<TContext>;
+  globalTranslationContext: (
+    ctx: TContext,
+  ) => Record<string, FluentVariable>;
+  defaultLocale: string;
+  directory: string;
+}) {
+  const i18n = new I18n<TContext>({
+    defaultLocale: defaultLocale,
+    directory: directory,
+    localeNegotiator: localeNegotiator,
+    globalTranslationContext: globalTranslationContext,
+  });
+  return i18n;
+}
