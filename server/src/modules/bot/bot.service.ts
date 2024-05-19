cc10b8e888cc9b1d09f5a8c951b68af0bc6ae81f
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-05-19 16:52:30 +0300

    update bot service

--- server/src/modules/bot/bot.service.ts
+++ server/src/modules/bot/bot.service.ts
@@ -1,10 +1,33 @@
+import { RunnerHandle, run } from '@grammyjs/runner';
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Bot } from 'grammy';
+import { MainContext } from './bot.interface';
+import { ConfigService } from '@nestjs/config';
+import { enableTranslator } from './helpers/translator.middleware';
+import { enableSequentionlize } from './helpers/sequentionlize.middleware';
 
 @Injectable()
 export class BotService implements OnModuleInit {
-  constructor() {}
+  private readonly bot: Bot<MainContext>;
+  private handler: RunnerHandle;
 
-  onModuleInit() {
-    throw new Error('Method not implemented.');
+  constructor(private readonly config: ConfigService) {
+    const telegramToken = config.get('TELEGRAM_TOKEN');
+
+    if (!telegramToken) throw Error('TELEGRAM_TOKEN is not exist');
+
+    this.bot = new Bot<MainContext>(telegramToken);
+    this.bot.use(enableSequentionlize());
+    this.bot.use(enableTranslator());
+
+    this.bot.on('message', async ctx => {
+      await ctx.reply(
+        ctx.t('start-welcome') + `, name: ${ctx.from.first_name}`,
+      );
+    });
+
+    this.handler = run(this.bot);
   }
+
+  onModuleInit() {}
 }
