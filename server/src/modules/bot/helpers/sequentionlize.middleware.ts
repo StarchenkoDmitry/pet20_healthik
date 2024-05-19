54f9470c99942297ea88b85009a8c24866ec88d0
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-05-20 00:49:04 +0300

    update: sequentionlize middleware

--- server/src/modules/bot/helpers/sequentionlize.middleware.ts
+++ server/src/modules/bot/helpers/sequentionlize.middleware.ts
@@ -1,9 +1,11 @@
-import { sequentialize } from "@grammyjs/runner";
-import { MiddlewareFn } from "grammy";
-import { MainContext } from "../bot.interface";
+import { sequentialize } from '@grammyjs/runner';
+import { MiddlewareFn } from 'grammy';
+import { MainContext } from '../bot.interface';
 
 export function enableSequentionlize(): MiddlewareFn<MainContext> {
-    return sequentialize<MainContext>(ctx => {
-      return ctx.from?.id.toString();
-    });
+  return sequentialize<MainContext>(ctx => {
+    const chatId = ctx.chat?.id;
+    const userId = ctx.from?.id;
+    return `${chatId ?? '' }/${userId ?? ''}`;
+  });
 }
