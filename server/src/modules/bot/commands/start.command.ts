903e6c5abee3095d089543f8afe4f8790f9fe1bc
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-05-25 17:11:14 +0300

    add: start command

---
+++ server/src/modules/bot/commands/start.command.ts
@@ -0,0 +1,15 @@
+import { CommandContext } from 'grammy';
+import { MainContext } from '../bot.interface';
+import { HandlerNameList } from 'src/modules/handler/handler-name-list';
+import { DEFAULT_HANDLER_NAME_AFTER_START } from 'src/config';
+
+export async function startCommand(ctx: CommandContext<MainContext>) {
+  if (ctx.session.currentHandler !== HandlerNameList.none) {
+    await ctx.reply(ctx.t('start-registered'));
+    return;
+  }
+
+  await ctx.reply(ctx.t('start-welcome'));
+  
+  await ctx.changeHandler(DEFAULT_HANDLER_NAME_AFTER_START);
+}
