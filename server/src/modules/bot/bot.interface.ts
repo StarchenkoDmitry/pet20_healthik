7958137993dafc7c426e56bda3e0c5e84b0eaa65
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-05-25 17:16:04 +0300

    update: bot interface

--- server/src/modules/bot/bot.interface.ts
+++ server/src/modules/bot/bot.interface.ts
@@ -1,7 +1,19 @@
 import { I18nFlavor } from "@grammyjs/i18n";
 import { Context, SessionFlavor } from "grammy";
 import { TelegramUser } from "../telegram-user/telegram-user.entity";
+import { ObjectLiteral, Repository } from "typeorm";
+import { HandlerNamesType } from "../handler/handler-name-list";
+
+
+export type GetDBRepositoryFunc = <T extends ObjectLiteral>(entityClass: ObjectLiteral) => Repository<T>;
+export type ChangeHandlerFunc = (name: HandlerNamesType)=>Promise<void>;
+
+export type ExtensionContext = {
+  changeHandler: (name: HandlerNamesType)=>Promise<void>;
+  getDBRepository: GetDBRepositoryFunc;
+}
 
 export type MainContext = Context 
-    & I18nFlavor 
-    & SessionFlavor<TelegramUser>;
+  & I18nFlavor
+  & SessionFlavor<TelegramUser> 
+  & ExtensionContext;
