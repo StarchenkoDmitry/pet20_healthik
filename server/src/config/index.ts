feb1af7ed1660f864297039ed5d8849acd175054
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-05-25 17:08:52 +0300

    update: config

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,5 +1,9 @@
-
+import { HandlerNameList, HandlerNamesType } from "src/modules/handler/handler-name-list";
 
 export const DEFAULT_I18N_LANGUAGE_CODE = 'en';
 export const DEFAULT_I18N_DIRECTORY = 'src/i18n/locales/';
 export const DEFAULT_I18N_FIRST_NAME = 'nameless';
+
+export const DEFAULT_HANDLER_NAME_ON_INITIALIZATION:HandlerNamesType = HandlerNameList.none;
+export const DEFAULT_HANDLER_NAME_AFTER_START:HandlerNamesType = HandlerNameList.mainMenu;
+export const DEFAULT_HANDLER_NAME:HandlerNamesType = HandlerNameList.mainMenu;
