da21c1c5f5f5aa8fd0c69d1d9c3549f23588e628
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-05-25 17:19:25 +0300

    change: bot service

--- server/src/modules/bot/bot.service.ts
+++ server/src/modules/bot/bot.service.ts
@@ -9,6 +9,14 @@ import { enableSession } from './helpers/session.middleware';
 import { TelegramUserService } from '../telegram-user/telegram-user.service';
 import { enableMessageHistory } from './helpers/messageHistory.middleware';
 import { TelegramMessageService } from '../telegram-message/telegram-message.service';
+import { enableExtensionContext } from './helpers/extensionContext.middleware.';
+import { startCommand } from './commands/start.command';
+import { restartCommand } from './commands/restart.command';
+import { mainMessageHandler } from '../handler/main-handler';
+import { TelegramMessage } from '../telegram-message/telegram-message.entity';
+import { TelegramUser } from '../telegram-user/telegram-user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class BotService implements OnModuleInit {
@@ -19,24 +27,40 @@ export class BotService implements OnModuleInit {
     private readonly config: ConfigService,
     private readonly telegramUserService: TelegramUserService,
     private readonly telegramMessageService: TelegramMessageService,
+
+    @InjectRepository(TelegramMessage)
+    private readonly telegramMessageRepository: Repository<TelegramMessage>,
+    @InjectRepository(TelegramUser)
+    private readonly telegramUsersRepository: Repository<TelegramUser>,
   ) {
     const telegramToken = config.get('TELEGRAM_TOKEN');
 
     if (!telegramToken) throw Error('TELEGRAM_TOKEN is not exist');
 
     this.bot = new Bot<MainContext>(telegramToken);
+
     this.bot.use(enableSequentionlize());
     this.bot.use(enableSession(telegramUserService));
-    this.bot.use(enableMessageHistory(telegramMessageService));
     this.bot.use(enableTranslator());
+    this.bot.use(
+      enableExtensionContext({
+        repositories: [
+          telegramMessageRepository,
+          telegramUsersRepository,
+        ],
+      }),
+    );
+    this.bot.use(enableMessageHistory(telegramMessageService));
 
-    this.bot.on('message', async ctx => {
-      await ctx.reply(
-        ctx.t('start-welcome') + `, name: ${ctx.from.first_name}`,
-      );
+    this.bot.command('start', startCommand);
+    this.bot.command('restart', restartCommand);
+    this.bot.on('message', mainMessageHandler);
+
+    this.handler = run(this.bot, {
+      runner: {
+        // silent:true,
+      },
     });
-
-    this.handler = run(this.bot);
   }
 
   onModuleInit() {}
