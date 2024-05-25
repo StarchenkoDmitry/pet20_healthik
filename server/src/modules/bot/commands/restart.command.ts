f2d11fbce4ca2340611c8453415f9a38148b766b
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-05-25 17:10:30 +0300

    add: restart command

---
+++ server/src/modules/bot/commands/restart.command.ts
@@ -0,0 +1,20 @@
+import { CommandContext } from 'grammy';
+import { MainContext } from '../bot.interface';
+import { 
+  DEFAULT_HANDLER_NAME, 
+  DEFAULT_I18N_LANGUAGE_CODE 
+} from 'src/config';
+
+
+export async function restartCommand(ctx: CommandContext<MainContext>) {
+  if(!ctx.from){ return; }
+  
+  ctx.session.id = ctx.from.id.toString();
+  ctx.session.firstName = ctx.from.first_name;
+  ctx.session.languageCode = ctx.from.language_code ?? DEFAULT_I18N_LANGUAGE_CODE;
+  ctx.i18n.useLocale(ctx.session.languageCode);
+
+  await ctx.reply(ctx.t("restart"));
+
+  await ctx.changeHandler(DEFAULT_HANDLER_NAME);
+}
