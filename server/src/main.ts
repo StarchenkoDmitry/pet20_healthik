a4cdfbd9b1f4e66fec45e4431f89b3d04ef5e85e
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-05-31 22:16:10 +0300

    feat: enable cookieParser

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,6 +9,12 @@ async function bootstrap() {
     origin: true,
     credentials: true,
   });
+
+  const config = app.get(ConfigService);
+
+  const cookie_secret = config.get("COOKIE_SECRET")
+  app.use(cookieParser(cookie_secret));
+
   await app.listen(3000);
 }
 bootstrap();
