12a2909ea9aaeef5f47dc584cb9f927bc8c8211a
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-05-31 23:19:49 +0300

    feat: add global ValidationPipe

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from "cookie-parser";
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,12 @@ async function bootstrap() {
 
   const cookie_secret = config.get("COOKIE_SECRET")
   app.use(cookieParser(cookie_secret));
+
+  app.useGlobalPipes(new ValidationPipe({
+    transform:true,
+    whitelist:true,
+    forbidNonWhitelisted:true
+  }));
 
   await app.listen(3000);
 }
