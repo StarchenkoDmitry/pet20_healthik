2b87acde565da6bd6eb3c3d66b6e2bfa67d2a460
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-05-31 21:23:21 +0300

    fix: add typeorm repositories in bot module

--- server/src/modules/bot/bot.module.ts
+++ server/src/modules/bot/bot.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { BotService } from './bot.service';
 import { TelegramUserModule } from '../telegram-user/telegram-user.module';
 import { TelegramMessageModule } from '../telegram-message/telegram-message.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TelegramUser } from '../telegram-user/telegram-user.entity';
+import { TelegramMessage } from '../telegram-message/telegram-message.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([TelegramUser, TelegramMessage]),
     TelegramUserModule,
-    TelegramMessageModule
+    TelegramMessageModule,
   ],
   providers: [BotService],
   exports: [BotService],
