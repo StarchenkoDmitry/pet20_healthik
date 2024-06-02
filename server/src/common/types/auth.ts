85579100f7e370ebce2cbdec3eb4f2db05b024d2
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-06-02 19:26:59 +0300

    refactor(common): auth types

--- server/src/common/types/auth.ts
+++ server/src/common/types/auth.ts
@@ -1,31 +1,11 @@
-import { Length, IsEmail } from 'class-validator';
-import {
-  MAX_EMAIL_LENGTH,
-  MAX_NAME_LENGTH,
-  MAX_PASSWORD_LENGTH,
-  MIN_EMAIL_LENGTH,
-  MIN_NAME_LENGTH,
-  MIN_PASSWORD_LENGTH,
-} from '../constants/auth';
-
-export class SignUpDto {
-  @Length(MIN_NAME_LENGTH, MAX_NAME_LENGTH)
+export class SignUpRequest {
   name!: string;
-
-  @IsEmail()
-  @Length(MIN_EMAIL_LENGTH, MAX_EMAIL_LENGTH)
   email!: string;
-
-  @Length(MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH)
   password!: string;
 }
 
-export class SignInDto {
-  @IsEmail()
-  @Length(MIN_EMAIL_LENGTH, MAX_EMAIL_LENGTH)
+export class SignInRequest {
   email!: string;
-
-  @Length(MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH)
   password!: string;
 }
 
