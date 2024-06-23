71efc734279a01db9fd974e78032d84bf5700664
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-06-23 16:47:24 +0300

    fix(server): cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,28 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import * as cookieParser from "cookie-parser";
+import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { GlobalExceptionHandler } from './shared/filters/global-exception.filter';
 import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ 
-    origin: true,
+
+  app.enableCors({
+    origin: 'http://127.0.0.1:4200',
     credentials: true,
   });
 
   const config = app.get(ConfigService);
 
-  const cookie_secret = config.get("COOKIE_SECRET")
+  const cookie_secret = config.get('COOKIE_SECRET');
   app.use(cookieParser(cookie_secret));
 
-  app.useGlobalPipes(new ValidationPipe({
-    transform:true,
-    whitelist:true,
-    forbidNonWhitelisted:true
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   app.useGlobalFilters(new GlobalExceptionHandler());
   app.useGlobalFilters(new HttpExceptionFilter());
