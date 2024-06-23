6bc727969e1d05e5e3938d823d905a05fb8180e9
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-06-23 16:37:24 +0300

    feat(common): add a constant to auth.ts

--- server/src/common/constants/auth.ts
+++ server/src/common/constants/auth.ts
@@ -4,3 +4,6 @@ export const MIN_EMAIL_LENGTH = 1;
 export const MAX_EMAIL_LENGTH = 128;
 export const MIN_PASSWORD_LENGTH = 1;
 export const MAX_PASSWORD_LENGTH = 256;
+
+//delay time for email verification
+export const EMAIL_VERIFICATION_DELAY_TIME_MS = 500;
