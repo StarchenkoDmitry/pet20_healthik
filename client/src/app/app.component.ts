82e8dbe2d560d823066b4d02094b11c6f586d61a
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-06-29 18:36:27 +0300

    update(client): set main-layout in root(app) page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, MainLayoutComponent],
 })
 export class AppComponent {}
