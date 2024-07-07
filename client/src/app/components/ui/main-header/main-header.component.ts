06a394fde6beea254bd19b0060d905938096cbec
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-07-08 00:25:51 +0300

    update(client): add profile-menu element

--- client/src/app/components/ui/main-header/main-header.component.ts
+++ client/src/app/components/ui/main-header/main-header.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { ProfileMenuComponent } from "../header/profile-menu/profile-menu.component";
 
 @Component({
-  selector: 'app-main-header',
-  standalone: true,
-  imports: [ButtonModule,CommonModule],
-  templateUrl: './main-header.component.html',
-  styleUrl: './main-header.component.scss',
+    selector: 'app-main-header',
+    standalone: true,
+    templateUrl: './main-header.component.html',
+    styleUrl: './main-header.component.scss',
+    imports: [ButtonModule, CommonModule, ProfileMenuComponent]
 })
 export class MainHeaderComponent {
   showMenu = false;
