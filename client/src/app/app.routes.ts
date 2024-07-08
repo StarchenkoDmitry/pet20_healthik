2f0fbfdc1bd247d19c343835c09fc00b38ba9769
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-07-08 03:20:54 +0300

    update(client): add pages in routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AboutUsPage } from '@pages/about-us-page/about-us-page.component';
+import { BecomeAdminPage } from '@pages/become-admin-page/become-admin-page.component';
+import { MainPageComponent } from '@pages/main-page/main-page.component';
+import { ProfilePage } from '@pages/profile-page/profile-page.component';
+import { SignupPageComponent } from '@pages/signup-page/signup-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'authorize', component: SignupPageComponent },
+  { path: 'become-admin', component: BecomeAdminPage },
+  { path: 'about-us', component: AboutUsPage },
+  { path: 'profile', component: ProfilePage },
+];
