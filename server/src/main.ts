93add4800862205ebbea0a5edb64bf986f134dab
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-07-08 03:02:32 +0300

    update(server): main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,19 +5,33 @@ import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { GlobalExceptionHandler } from './shared/filters/global-exception.filter';
 import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
+import { parseBoolean } from './shared/utils/parser/boolean-parser';
+import { 
+  COOKIE_SECRET_TOKEN, 
+  CORS_DEV_TOKEN, 
+  CORS_ORIGIN_TOKEN 
+} from './config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: 'http://127.0.0.1:4200',
-    credentials: true,
-  });
-
   const config = app.get(ConfigService);
 
-  const cookie_secret = config.get('COOKIE_SECRET');
-  app.use(cookieParser(cookie_secret));
+  const CORS_DEV = parseBoolean(config.get(CORS_DEV_TOKEN), {default:false});
+  if(CORS_DEV){
+    app.enableCors({
+      origin: true,
+      credentials: true,
+    });
+  }else{
+    const CORS_ORIGIN = config.get(CORS_ORIGIN_TOKEN);
+    app.enableCors({
+      origin: CORS_ORIGIN,
+      credentials: true,
+    });
+  }
+
+  const COOKIE_SECRET = config.get(COOKIE_SECRET_TOKEN);
+  app.use(cookieParser(COOKIE_SECRET));
 
   app.useGlobalPipes(
     new ValidationPipe({
