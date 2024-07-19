51801e4a0fb26150ea9f584edfafc1d014a0525e
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-07-19 21:15:56 +0300

    update(client): add new routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { AboutUsPage } from '@pages/about-us-page/about-us-page.component';
 import { BecomeAdminPage } from '@pages/become-admin-page/become-admin-page.component';
+import { ConversationPage } from '@pages/conversation/conversation.component';
 import { MainPageComponent } from '@pages/main-page/main-page.component';
+import { NewConversationPage } from '@pages/new-conversation/new-conversation.component';
 import { ProfilePage } from '@pages/profile-page/profile-page.component';
 import { SignupPageComponent } from '@pages/signup-page/signup-page.component';
 
@@ -11,4 +13,6 @@ export const routes: Routes = [
   { path: 'become-admin', component: BecomeAdminPage },
   { path: 'about-us', component: AboutUsPage },
   { path: 'profile', component: ProfilePage },
+  { path: 'new-conversation', component: NewConversationPage },
+  { path: 'conversation/:chatId', component: ConversationPage },
 ];
