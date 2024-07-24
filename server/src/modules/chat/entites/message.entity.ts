036429a2994babe0bde4c5ad234e7a8fae33b080
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-07-24 17:16:34 +0300

    ref(server): message entity

--- server/src/modules/chat/entites/message.entity.ts
+++ server/src/modules/chat/entites/message.entity.ts
@@ -1,33 +1,36 @@
 import {
-    Column,
-    CreateDateColumn,
-    Entity,
-    JoinColumn,
-    ManyToOne,
-    PrimaryColumn,
-  } from 'typeorm';
-  import { Chat } from './chat.entity';
-  import { User } from '../../user/user.entity';
-  
-  @Entity({ name: 'messages' })
-  export class Message {
-    @PrimaryColumn({ type: 'bigint' })
-    id: string;
-  
-    @PrimaryColumn({ type: 'uuid' })
-    chatId: string;
-  
-    @ManyToOne(() => Chat, chat => chat.messages)
-    @JoinColumn({ name: 'chatId', referencedColumnName: 'id' })
-    chat: Chat;
-  
-    @ManyToOne(() => User)
-    user: User;
-  
-    @Column({ type: 'varchar' })
-    text: string;
-  
-    @CreateDateColumn()
-    created_at: Date;
-  }
-  
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from 'typeorm';
+import { Chat } from './chat.entity';
+import { User } from '../../user/user.entity';
+
+@Entity({ name: 'messages' })
+export class Message {
+  @PrimaryColumn({ type: 'bigint' })
+  id: string;
+
+  @PrimaryColumn({ type: 'uuid' })
+  chatId: string;
+
+  @ManyToOne(() => Chat, chat => chat.messages)
+  @JoinColumn({ name: 'chatId', referencedColumnName: 'id' })
+  chat: Chat;
+
+  @Column({ type: 'uuid' })
+  userId: string;
+
+  @ManyToOne(() => User, user => user.myMessages)
+  @JoinColumn({ name: "userId", referencedColumnName: 'id'})
+  user: User;
+
+  @Column({ type: 'varchar' })
+  text: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+}
